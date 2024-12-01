20eb4f9c21f2dca2792122f959da9a0eab805ae5
AleOnta <casualotn@gmail.com>
2024-12-01 21:32:19 +0100

    initialized the project

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ale-o/php-framework',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '39acf97ea3a4fb54a5c8c2365a6b3f4730b0b5db',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ale-o/php-framework' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '39acf97ea3a4fb54a5c8c2365a6b3f4730b0b5db',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
