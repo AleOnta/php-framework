c292f7c6e26c1df4d07b4590157706e4691eb887
AleOnta <casualotn@gmail.com>
2024-12-03 22:35:09 +0100

    adds AppConstants class for handling abs path in the app

--- src/app/utility/AppConstants.php
+++ src/app/utility/AppConstants.php
@@ -5,11 +5,12 @@ namespace App\Utility;
 class AppConstants
 {
     # define the root path of the application
-    public const ROOT_DIR = __DIR__ . "/../../../";
+    public const ROOT_DIR = 'C:/Users/Ale-O/Documents/Projects/php-framework/';
     # define other child paths
     public const ENV_PATH = self::ROOT_DIR . ".env";
     public const VIEWS_DIR = self::ROOT_DIR . "src/app/views/";
     public const ASSETS_DIR = self::ROOT_DIR . "public/assets/";
+    public const ROUTES_DIR = self::ROOT_DIR . "src/app/routes/";
     public const MIGRATIONS_DIR = self::ROOT_DIR . "src/app/migrations/";
     public const TEMPLATES_DIR = self::ROOT_DIR . "src/app/views/templates/";
     public const COMPONENTS_DIR = self::ROOT_DIR . "src/app/views/components/";
