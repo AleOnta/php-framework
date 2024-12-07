0f90f8292b33bd546618a2c8e3c7be2ace34ca4a
AleOnta <casualotn@gmail.com>
2024-12-07 14:12:03 +0100

    edit to the users migration

--- src/app/migrations/20240612_create_users_table.php
+++ src/app/migrations/20240612_create_users_table.php
@@ -10,7 +10,10 @@ class CreateUsersTable
             lastname VARCHAR(50) NOT NULL,
             username VARCHAR(50) NOT NULL UNIQUE,
             email VARCHAR(75) NOT NULL UNIQUE,
-            dob DATE NOT NULL
+            dob DATE NOT NULL,
+            status TINYINT NOT NULL DEFAULT 1,
+            created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         ) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;";
         # execute the query
         $db->exec($query);
