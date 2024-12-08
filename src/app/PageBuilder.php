13b48045223ddbfa23dab17ab54eaeaccb40533e
AleOnta <casualotn@gmail.com>
2024-12-08 12:47:23 +0100

    adds pagebuilder components loading

--- src/app/PageBuilder.php
+++ src/app/PageBuilder.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use App\Utility\AppConstants;
+use RuntimeException;
 
 class PageBuilder
 {
@@ -36,6 +37,8 @@ class PageBuilder
         $this->page = str_replace('{{styles}}', $styles, $this->page);
         # insert the main content of the page
         $this->page = str_replace('{{main}}', $main, $this->page);
+        # eventually load components into the page
+        $this->loadComponents();
         # append the js assets to the html
         $this->page = str_replace('{{js}}', $js, $this->page);
         # append building time to the html
@@ -44,8 +47,8 @@ class PageBuilder
         return $this->page;
     }
 
-    # method that analyze the assets requested and 
-    # loads them into the html page to return
+    # method that analyze the requested assets and 
+    # loads them into the html page to be rendered
     private function loadAssets($type, $assets)
     {
         $tags = '';
@@ -67,9 +70,73 @@ class PageBuilder
         return $tags;
     }
 
+    # checks if the page requires the rendering
+    # of components
+    private function loadComponents()
+    {
+        $matches = [];
+        $componentPattern = '/\{\{component:([a-zA-Z0-9_]+)(?:\.([a-zA-Z0-9_]+))?(?:\s(\{.*?\}))?\}\}/';
+        while (preg_match($componentPattern, $this->page, $matches)) {
+
+            # get the component path
+            $structure = count($matches);
+            $path = AppConstants::COMPONENTS_DIR;
+            if (isset($matches[2])) {
+                $path .= ucfirst($matches[1]) . '/';
+            }
+
+            # include the filename into the path
+            $file = match ($structure) {
+                2 => $path . basename($matches[1]),
+                3, 4 => $path . basename($matches[2])
+            };
+
+            # extract eventual parameters for the component
+            $attributes = [];
+            if (!empty($matches[3])) {
+                $attributes = json_decode($matches[3], true);
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    throw new RuntimeException("Invalid JSON in component attributes: " . json_last_error_msg());
+                }
+            }
+
+            # load the component into the page
+            $this->getComponent($matches[0], $path, $file, $attributes);
+        }
+    }
+
+    # retrieve and load the component into the html page requested
+    private function getComponent($element, $path, $file, $attributes)
+    {
+
+        # check if the component its html type (no logic to execute)
+        if (file_exists($file . '.html') && strpos(realpath($file . '.html'), realpath($path)) === 0) {
+            $component = file_get_contents($file);
+            $this->page = str_replace($element, $component, $this->page);
+            return;
+        }
+
+        # check if the component it's php type (requires execution)
+        if (file_exists($file . '.php') && strpos(realpath($file . '.php'), realpath($path)) === 0) {
+            ob_start();
+            # if any dynamic attribute is passed - extract
+            extract($attributes);
+            # use include to execute possible php logic in it
+            include $file . '.php';
+            # get all the content
+            $component = ob_get_clean();
+            # insert the component
+            $this->page = str_replace($element, $component, $this->page);
+            return;
+        }
+
+        # Insert comment in HTML for component not found
+        $this->page = str_replace($element, "<!-- COMPONENT {" . str_replace(['{{', '}}'], '', $element) . "} NOT FOUND -->", $this->page);
+    }
+
     private function getBuildTime()
     {
         # append to the html a s counter for the page building process
-        $this->page .= "<h2 class='absolute bottom-0 left-0'>Build time: " . (microtime(true) - $this->start) . "s</h2>";
+        $this->page .= "<h6 class='absolute bottom-0 left-0 text-xs text-slate-500'>Build time: " . (microtime(true) - $this->start) . "s</h6>";
     }
 }
--- src/app/views/index.php
+++ src/app/views/index.php
@@ -1,30 +1,5 @@
 <!-- START OF HOME COMPONENT -->
-<div class="contanier w-full h-screen flex flex-col items-center justify-center">
+<div class="contanier w-full h-screen flex flex-col items-center justify-center text-slate-50">
     {{component:navbar}}
-    <form action="/message/create-message" method="POST" class="w-2/6 text-center py-6 px-6 rounded-lg flex flex-col justify-between">
-
-        {{component:inputs.checkboxes}}
-
-        <div class="flex flex-row items-center justify-between mb-8">
-
-            {{component:inputs.datepicker}}
-
-            {{component:inputs.timepicker}}
-
-        </div>
-        <div class="py-3 px-4 rounded-lg bg-neutral-300 text-start shadow-lg">
-            <label class="block text-start text-sm text-gray-700 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mb-2" for="message">Message:</label>
-            <textarea
-                id="message"
-                name="message"
-                placeholder="Write your message here ..."
-                class="w-full h-60 p-3 rounded-md"></textarea>
-        </div>
-        <button
-            class="w-full bg-teal-700 py-3 mt-6 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-100 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:bg-teal-500 before:border-t-teal-700 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-[#fff]">
-            Send Message
-        </button>
-    </form>
-
 </div>
 <!-- END OF HOME COMPONENT -->
