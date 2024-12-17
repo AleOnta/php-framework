c166192879e09370b688454729b8e8801e982c1f
AleOnta <casualotn@gmail.com>
2024-12-17 22:45:11 +0100

    implement Middleware exec in router

--- src/app/Router.php
+++ src/app/Router.php
@@ -11,45 +11,50 @@ class Router
     protected $defaultRoute;
     protected $groupPrefix = '';
 
-    private function addRoute($route, $controller, $action, $method)
+    private function addRoute($route, $controller, $action, $method, $middlewares)
     {
+        if ($route === '/users/login') {
+            print_r($middlewares);
+            die();
+        }
         $fullRoute = $this->groupPrefix . $route;
         $this->routes[$method][$fullRoute] = [
             'controller' => $controller,
             'action' => $action,
+            'middleware' => $middlewares,
             # parameters will be implemented in the future
             'params' => []
         ];
     }
 
     # Creates a new GET route in the application
-    public function get($route, $controller, $action)
+    public function get($route, $controller, $action, $middlewares = [])
     {
-        $this->addRoute($route, $controller, $action, 'GET');
+        $this->addRoute($route, $controller, $action, 'GET', $middlewares);
     }
 
     # creates a new POST route in the application
-    public function post($route, $controller, $action)
+    public function post($route, $controller, $action, $middlewares = [])
     {
-        $this->addRoute($route, $controller, $action, 'POST');
+        $this->addRoute($route, $controller, $action, 'POST', $middlewares);
     }
 
     # creates a new PUT route in the application
-    public function put($route, $controller, $action)
+    public function put($route, $controller, $action, $middlewares = [])
     {
-        $this->addRoute($route, $controller, $action, 'PUT');
+        $this->addRoute($route, $controller, $action, 'PUT', $middlewares);
     }
 
     # creates a new PATCH route in the application
-    public function patch($route, $controller, $action)
+    public function patch($route, $controller, $action, $middlewares = [])
     {
-        $this->addRoute($route, $controller, $action, 'PATCH');
+        $this->addRoute($route, $controller, $action, 'PATCH', $middlewares);
     }
 
     # creates a new DELETE route in the application
-    public function delete($route, $controller, $action)
+    public function delete($route, $controller, $action, $middlewares = [])
     {
-        $this->addRoute($route, $controller, $action, 'DELETE');
+        $this->addRoute($route, $controller, $action, 'DELETE', $middlewares);
     }
 
     # creates a default fallback route for specific use-cases
@@ -78,24 +83,57 @@ class Router
         $method = $_SERVER['REQUEST_METHOD'];
 
         # check if the route exists
-        if (array_key_exists($uri, $this->routes[$method])) {
+        if (isset($this->routes[$method][$uri])) {
+            # extract the route
+            $route = $this->routes[$method][$uri];
             # get the related controller
-            $controller = $this->routes[$method][$uri]['controller'];
+            $controller = $route['controller'];
             # get the related action
-            $action = $this->routes[$method][$uri]['action'];
-            # execute the correct action
-            $controller = new $controller();
-            $controller->$action();
+            $action = $route['action'];
+            # get the middleware stack or empty arr
+            $middlewareStack = $route['middleware'] ?? [];
+            # execute the middleware before invoking the action
+            $this->runMiddleware($middlewareStack, function () use ($controller, $action) {
+                $controller = new $controller();
+                $controller->$action();
+            });
         } else {
-            # check if a default route is set
-            if ($this->defaultRoute) {
-                # create the default controller
-                $controller = new $this->defaultRoute['controller'];
-                $controller->$this->defaultRoute['action'];
-            } else {
-                # return 404 response
-                $this->handle404();
-            }
+            # redirect to default or 404
+            $this->handleRouteNotFound();
+        }
+    }
+
+    # run the first middleware inside the middleware stack
+    private function runMiddleware($middlewareStack, $next)
+    {
+        # if no middleware is defined, proceed w req. handling
+        if (empty($middlewareStack)) {
+            $next();
+            return;
+        }
+
+        # get the first required middleware
+        $middlewareClass = array_shift($middlewareStack);
+        # create instance of that middleware
+        $middleware = new $middlewareClass();
+        # run the middleware and pass the remaining stack
+        $middleware->handle($_SERVER, function () use ($middlewareStack, $next) {
+            # run the middleware til stack is empty
+            $this->runMiddleware($middlewareStack, $next());
+        });
+    }
+
+    # handle request not found
+    private function handleRouteNotFound()
+    {
+        # check if a default route is set
+        if ($this->defaultRoute) {
+            # create the default controller
+            $controller = new $this->defaultRoute['controller'];
+            $controller->$this->defaultRoute['action'];
+        } else {
+            # return 404 response
+            $this->handle404();
         }
     }
 
