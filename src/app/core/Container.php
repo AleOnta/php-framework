2a979a2726b9bbb799ab800be2ea1987dcad5c73
AleOnta <casualotn@gmail.com>
2024-12-21 17:21:14 +0100

    adds Container class

---
+++ src/app/core/Container.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Core;
+
+class Contanier
+{
+    private array $services = [];
+
+    # store dependencies in the container
+    public function set(string $key, callable $resolver)
+    {
+        $this->services[$key] = $resolver;
+    }
+
+    # return the requested dependencies if availables
+    public function get($key)
+    {
+        if (!isset($this->services[$key])) {
+            throw new \Exception("Service '{$key}' not found.");
+        }
+        return $this->services[$key]($this);
+    }
+}
