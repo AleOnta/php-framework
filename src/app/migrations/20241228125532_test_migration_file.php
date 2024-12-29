d82fc64d91d7b16cf4419436a4197de2d4f68251
AleOnta <casualotn@gmail.com>
2024-12-29 14:52:19 +0100

    adds fake migration file

---
+++ src/app/migrations/20241228125532_test_migration_file.php
@@ -0,0 +1,16 @@
+<?php
+
+class TestMigrationFile
+{
+    public function up()
+    {
+        # fakes db operations
+        sleep(3);
+    }
+
+    public function down()
+    {
+        # fake db operations
+        sleep(3);
+    }
+}
